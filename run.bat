02d1336ff16397547487b47256e477eabe9d0c3f
Mh nikkhoofard <nikkhoofard2000@gmail.com>
2025-05-19 14:07:34 +0330

    Fix run.bat

--- run.bat
+++ run.bat
@@ -1,17 +1,8 @@
 
-@echo off
-
-REM Set the path to your virtual environment
-set VENV_PATH=D:\programing\python\online_shop
-
-REM Activate the virtual environment
-call "%VENV_PATH%\Scripts\activate.bat"
+call "D:\programing\python\online_shop\venv\Scripts\activate.bat"
 
 REM CD IN DIR
 cd D:\programing\python\online_shop\online-shop-django
 
 REM Run the Django development server
 py manage.py runserver
-
-REM Pause to keep the terminal open
-pause
--- shop/views.py
+++ shop/views.py
@@ -8,7 +8,7 @@ from cart.forms import QuantityForm
 
 
 def categories_processor(request):
-    categories = Category.objects.all()
+    categories = Category._default_manager.all()
     return {'categories': categories}
 
 
@@ -17,7 +17,7 @@ def paginat(request, list_objects):
 	page_number = request.GET.get('page')
 	try:
 		page_obj = p.get_page(page_number)
-	except PageNotAnInteger:
+	except django.core.paginator.PageNotAnInteger:
 		page_obj = p.page(1)
 	except EmptyPage:
 		page_obj = p.page(p.num_pages)
